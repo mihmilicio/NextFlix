5e455c71fa2b5e9154c1ca74d487991529449755
mihmilicio <milenamilicio@hotmail.com>
2024-05-10 19:11:38 -0300

    build: adiciona coil

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,11 +61,15 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.material.icons)
+    implementation(libs.io.coil.kt.compose)
+
     testImplementation(libs.junit)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
