a56b1399a38af4f9388d582c91eae3151f4913cf
mihmilicio <milenamilicio@hotmail.com>
2024-05-12 20:34:47 -0300

    feat: adiciona loading no catálogo

---
+++ app/src/main/java/io/github/mihmilicio/nextflix/ui/components/LoadingView.kt
@@ -0,0 +1,19 @@
+package io.github.mihmilicio.nextflix.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Loader(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator()
+    }
+}
--- app/src/main/java/io/github/mihmilicio/nextflix/ui/features/catalogo/CatalogoListView.kt
+++ app/src/main/java/io/github/mihmilicio/nextflix/ui/features/catalogo/CatalogoListView.kt
@@ -2,15 +2,23 @@ package io.github.mihmilicio.nextflix.ui.features.catalogo
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
+import androidx.compose.foundation.lazy.grid.LazyGridScope
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.paging.CombinedLoadStates
+import androidx.paging.LoadState
 import androidx.paging.PagingData
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import io.github.mihmilicio.nextflix.data.Serie
+import io.github.mihmilicio.nextflix.ui.components.Loader
 import io.github.mihmilicio.nextflix.ui.theme.NextFlixTheme
 import kotlinx.coroutines.flow.flowOf
 
@@ -25,7 +33,38 @@ fun SerieGrid(seriePagingItems: LazyPagingItems<Serie>) {
         items(seriePagingItems.itemCount) { i ->
             SerieCard(seriePagingItems[i]!!)
         }
-        // TODO loadStates
+        renderizarEstados(seriePagingItems.loadState)
+    }
+}
+
+fun LazyGridScope.renderizarEstados(loadState: CombinedLoadStates) {
+    loadState.apply {
+        when {
+            refresh is LoadState.Loading -> {
+                item(span = { GridItemSpan(maxLineSpan) }) {
+                    Loader(
+                        modifier = Modifier.fillMaxSize()
+                    )
+                }
+            }
+
+            refresh is LoadState.Error -> {
+                // TODO
+            }
+
+            append is LoadState.Loading -> {
+                item(span = { GridItemSpan(maxLineSpan) }) {
+                    Loader(
+                        modifier = Modifier.padding(top = 12.dp)
+                    )
+
+                }
+            }
+
+            append is LoadState.Error -> {
+                // TODO
+            }
+        }
     }
 }
 
@@ -33,6 +72,8 @@ fun SerieGrid(seriePagingItems: LazyPagingItems<Serie>) {
 @Composable
 fun SerieGridPreview() {
     NextFlixTheme {
-        SerieGrid(flowOf(PagingData.from(Serie.listaStub)).collectAsLazyPagingItems())
+        SerieGrid(
+            flowOf(PagingData.from(Serie.listaStub)).collectAsLazyPagingItems()
+        )
     }
 }
