f90cb6d2ddd9861e2f5e3f6edef5c2aef0a35862
mihmilicio <milenamilicio@hotmail.com>
2024-05-13 23:24:39 -0300

    feat: adiciona topBar na watchlist

--- app/src/main/java/io/github/mihmilicio/nextflix/ui/components/TopAppBar.kt
+++ app/src/main/java/io/github/mihmilicio/nextflix/ui/components/TopAppBar.kt
@@ -5,7 +5,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -38,4 +42,19 @@ fun TopAppBarSurfacePreview() {
             )
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NextFlixAppBar(
+    titulo: String,
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        title = { Text(titulo) },
+        modifier = modifier,
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.surfaceContainer
+        )
+    )
 }
--- app/src/main/java/io/github/mihmilicio/nextflix/ui/features/watchlist/WatchlistScreen.kt
+++ app/src/main/java/io/github/mihmilicio/nextflix/ui/features/watchlist/WatchlistScreen.kt
@@ -7,9 +7,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import io.github.mihmilicio.nextflix.R
+import io.github.mihmilicio.nextflix.ui.components.NextFlixAppBar
 import io.github.mihmilicio.nextflix.ui.theme.NextFlixTheme
 
 @Composable
@@ -19,7 +22,10 @@ fun WatchlistScreen(
 ) {
     Scaffold(
         modifier = modifier,
-        contentWindowInsets = WindowInsets(bottom = 0.dp)
+        contentWindowInsets = WindowInsets(bottom = 0.dp),
+        topBar = {
+            NextFlixAppBar(stringResource(R.string.watchlist_titulo))
+        }
     ) { innerPadding ->
         Column(
             modifier = Modifier
