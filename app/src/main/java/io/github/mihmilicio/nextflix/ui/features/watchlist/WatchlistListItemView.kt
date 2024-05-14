509926a40300b52f7869251bf3687de68130a321
mihmilicio <milenamilicio@hotmail.com>
2024-05-13 23:30:14 -0300

    feat: troca checkbox de episódio por botão

--- app/src/main/java/io/github/mihmilicio/nextflix/ui/features/watchlist/WatchlistListItemView.kt
+++ app/src/main/java/io/github/mihmilicio/nextflix/ui/features/watchlist/WatchlistListItemView.kt
@@ -4,7 +4,10 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Checkbox
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.FilledTonalIconButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
@@ -12,10 +15,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import io.github.mihmilicio.nextflix.R
 import io.github.mihmilicio.nextflix.domain.model.SerieParaAssistir
 import io.github.mihmilicio.nextflix.ui.theme.NextFlixTheme
 
@@ -61,9 +66,12 @@ fun EpisodioCard(
                 }
             },
             trailingContent = {
-                Checkbox(
-                    checked = false,
-                    onCheckedChange = { marcarEpisodioAssistido(serie) })
+                FilledTonalIconButton(onClick = { marcarEpisodioAssistido(serie) }) {
+                    Icon(
+                        Icons.Default.Check,
+                        contentDescription = stringResource(R.string.watchlist_btn_assistido_content_description)
+                    )
+                }
             }
         )
     }
